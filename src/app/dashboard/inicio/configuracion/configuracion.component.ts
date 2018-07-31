6516f14f43c17907c3f37fc45872dbbe5827ee1f
kuper2018 <alejandro_N_@hotmail.com>
2018-07-31 18:57:59 -0400

    mi primer Comit

---
+++ src/app/dashboard/inicio/configuracion/configuracion.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {Datos} from './Datos';
+
+
+
+
+
+@Component({
+  selector: 'app-configuracion',
+  templateUrl: './configuracion.component.html',
+  styleUrls: ['./configuracion.component.scss']
+})
+export class ConfiguracionComponent implements OnInit {
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+   
+}
---
+++ src/app/portal/mihogar/candidato-h/buscar-h/buscar-h.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-h',
+  templateUrl: './buscar-h.component.html',
+  styleUrls: ['./buscar-h.component.scss']
+})
+export class BuscarHComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
